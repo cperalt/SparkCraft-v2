fc67ba877cff32aea686a4de287e45645e73e4c1
cperalt <145234956+cperalt@users.noreply.github.com>
2024-07-16 22:07:17 -0400

    Created Not Found Page

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (
+        <div className="text-yellow-500 text-9xl">
+            404 Error! The page you are looking for is not found!
+        </div>
+    )
+}
+
+export default NotFound
