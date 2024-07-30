35a13e067031b342680546f0235ec2bcd1c586f6
cperalt <145234956+cperalt@users.noreply.github.com>
2024-07-30 12:18:50 -0400

    updates

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -11,11 +11,11 @@ function Home() {
                         <source src={hero} type="video/mp4"/>
                     </video>
                     <div className={styles.headerBrand}>
-                        <p className={styles.headerText}>crafting the robots of tommorow</p>
+                        <p className={`${styles.headerText}`}>crafting the robots of tommorow</p>
                     </div>
                 </div>
             </div>
-            <main className={styles.mainSection}>
+            <main className={`${styles.mainSection}`}>
                 <section className={styles.firstSection}>
                     <div className={styles.firstSectionPara}>
                         <p className={styles.sectionStatement}><span className={styles.sectionStatementWord}>Future-oriented</span> and <span className={styles.sectionStatementWord}>creative solutions</span> to empower and assist you in today's fast-paced world.</p>
